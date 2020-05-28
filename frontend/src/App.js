7d60b64dc3167a558cb1c4f5d28ffa81a81edc43
Lavi <laviionas772@gmail.com>
2020-05-28 10:09:22 -0400

    Updates time by the second

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,63 @@
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  const [currentTime, setCurrentTime] = useState(0);
+class App extends Component {
+  constructor() {
+    super();
 
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, []);
+    this.state = {
+      time : 0
+    };
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-        <p>The current time is {currentTime}.</p>
-      </header>
-    </div>
-  );
+  fetchTime () {
+    fetch('/time')
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({time: data.time});
+      });
+  }
+
+  componentDidMount() {
+
+    this.interval = setInterval(() => this.fetchTime(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div>Time: {this.state.time}</div>
+    );
+  }
 }
+
+//
+// function App() {
+//   const [currentTime, setCurrentTime] = useState(0);
+//
+//   useEffect(() => {
+//     fetch('/time').then(res => res.json()).then(data => {
+//       setCurrentTime(data.time);
+//     });
+//   }, []);
+//
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+//           Learn React
+//         </a>
+//         <p>The current time is {currentTime}.</p>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
